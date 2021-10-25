caea959d291e69f7902ba555fa6be5bf9c1ceba0
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-10-25 17:57:46 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,26 +1,20 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "test";
-
-// Create connection
-
-$conn = new mysqli('localhost','root','','test');
-$firstname = ($_POST["name"]);
-$email = ($_POST["email"]);
-$comment = ($_POST["comment"]);
-$gender = ($_POST["gender"]);
-if($conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
-{
-    echo "New record created successfully";
-    $stmt = $conn->prepare("INSERT INTO users(f_name, email, comment, gender) values('$firstname', '$email', '$comment', '$gender')");
-    $execval = $stmt->execute();
-    $stmt->close();
-    $conn->close();
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'test');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 else
 {
-    die("failed to connect !");
+    echo "connection success !";
 }
 ?>
