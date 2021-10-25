e61de26e574ae5af15ccec707b92606cc1e410f2
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-10-25 17:58:18 +0200

    Add files via upload

--- Register.php
+++ Register.php
@@ -8,11 +8,53 @@
 // define variables and set to empty values
 
 $firstname = $email = $gender = $comment = $website = "";
-
+$ct1 = 0;
+// Include config file
+require_once "connection.php";
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $firstname = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $comment = trim($_POST["comment"]);
+  $gender = trim($_POST["gender"]);
+  if(empty($firstname) && empty($email)&& empty($comment) && empty($gender))
+  {
+      $name_err = "Please enter a valid name.";
+      $email_err = "Please enter a valid email.";
+      $comment_err = "Please enter a valid comment.";
+      $gender_err = "Please enter a valid gender.";
+  }
+  else
+  {
+    $sql = ("INSERT INTO users(f_name, email, comment, gender) VALUES('$firstname', '$email', '$comment', '$gender')");
+    if($stmt = mysqli_prepare($link, $sql))
+    {
+            if(mysqli_stmt_execute($stmt))
+            {
+              // Records created successfully. Redirect to landing page
+              echo "Records added successfully.";
+              header("location: n.html");
+              exit();
+            } 
+            else 
+            {
+                echo "Something went wrong. Please try again later.";
+            }
+    }
+    else
+    {
+        echo "Something went wrong. Please try again later.";
+    }
+    // Close statement
+    mysqli_stmt_close($stmt);
+  }
+  // Close connection
+  mysqli_close($link);
+}
 ?>
 
 <h2>PHP Form Validation Example</h2>
-<form action="connection.php" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
   Name: <input type="text" name="name"required>
   <br><br>
   E-mail: <input type="text" name="email"required>
