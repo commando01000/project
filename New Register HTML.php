3bffed8d2abd76c410929fe1fb0056502138e455
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-05 00:04:09 +0200

    Add files via upload

---
+++ New Register HTML.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+</head>
+<body>  
+
+
+
+<h1>Register</h1>
+ <p>Please fill in this form to create an account.</p>
+
+<form method="post" action="Untitled-2.php">
+<div class="container">
+<hr>
+  Name of patient: <input type="text" placeholder="Enter Name of patient" name="name of patient"required>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="Female">Female
+  <input type="radio" name="gender" value="Male">Male
+  <br><br>
+  Age of patient: <input type="text" placeholder="Enter age of patient" name="age of patient"required>
+  <br><br>
+  Patient jop: <input type="text" placeholder="Enter patient jop" name="patient jop"required>
+  <br><br>
+  Social status: <input type="text" name="Enter Social status" placeholder="Enter  Social status"required>
+  <br><br>
+  Rank in the family: <input type="Rank in the family" placeholder="Enter Rank in the family" required>
+
+
+
+
+  <br><br>
+  Complaints of family and companions: <textarea name="Eneter Complaints of family and companions" placeholder="Enter Complaints of family and companions" rows="5" cols="40"required></textarea>
+  <br><br>
+  <input type="submit" class="registerbtn" name="submit" value="Submit">  
+  <hr>
+  </div>
+</form>
+
+</body>
+</html>
