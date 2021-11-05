70a214163b10efd445e813960007a730a9457433
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-05 02:21:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,25 @@
 <head>
 </head>
 <body>  
+  
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
 
+<ul>
+    <li><a class="active" href="#home"></a></li>
+    <li><a href="#news">  </a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
+  
 
-
-<h3>Please click on Sign in if you have an account if not click on Sign up.</h3>
 
 <form method="post" action="LoginFA.php">
 <div class="container">
+  
+<h3>Please click on Sign in if you have an account if not click on Sign up.</h3>
+
+
 <hr>
   
   <input type="submit" class="registerbtn" name="submit" value="Sign in">  
