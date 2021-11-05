a28b84e23bbc93f65930cddf73f145a79cb166a2
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-05 02:20:48 +0200

    Update afterlogin.php

--- afterlogin.php
+++ afterlogin.php
@@ -3,12 +3,21 @@
 </head>
 <body>  
 
-
-<h1>Login </h1>
- <p>Please fill in this form to create an account.</p>
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+    <li><a class="active" href="#home"></a></li>
+    <li><a href="#news">  </a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
 
 <form method="post" action="regFP.php">
 <div class="container">
+<h1>Login </h1>
+ <p>Please fill in this form to create an account.</p>
+
+
 <hr>
 <br><br>
   <input type="submit" class="registerbtn" name="submit" value="Make profile for patient">  
