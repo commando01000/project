2f51d92de9ae2e8bb23ae1151a6e9eac3332e3b6
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-10 14:17:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+</head>
+<body>  
+  
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+
+<ul>
+  <li><a href="index"></a></li>
+  <li><a href="#news">  </a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
+  
+
+
+<form method="post" action="LoginFA.php">
+<div class="container">
+  
+<h3>Home</h3>
+
+
+<hr>
+  
+  <input type="submit" class="registerbtn" name="submit" value="Sign in">  
+  <hr>
+  </div>
+</form>
+
+</body>
+</html>
