85ffda21b977d82ba67a0b4c694dc0e75f1db0fa
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-10 10:07:35 +0200

    Add files via upload

---
+++ don.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+</head>
+<body>  
+
+
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="afterlogin">Choose your action</a></li>
+  </ul>
+
+<form method="post" action="PQ">
+<div class="container">
+
+<h1>Make profile for Donator</h1>
+
+<hr>
+  Name of Donator: <input type="text" placeholder="Enter Name of Donator" name="name of donator"required>
+  <br><br>
+  Donator no: <input type="number" placeholder="Enter Donator no" name="donator no"required>
+  <br><br>
+  Address: <input type="text" placeholder="Enter Address" name="Address"required>
+  <br><br>
+
+      <input type="submit" class="registerbtn" name="submit" value="Create">  
+      <hr>
+
+  <hr>
+</div>
+</form>
+</body>
+</html>
