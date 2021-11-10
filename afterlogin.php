8d0d0723efcb98746b22b62c94c3bea7e704c3a6
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-10 10:24:14 +0200

    Add files via upload

---
+++ afterlogin.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+</head>
+<body>  
+
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
+
+<form method="post" action="regFP.php">
+<div class="container">
+<h1>Choose Your Action </h1>
+
+<hr>
+
+
+  <input type="submit" class="registerbtn" name="submit" value="Make profile for patient">  
+  <hr>
+  </div>
+</form>
+<form method="post" action="regFA.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="Add Servant">  
+      <hr>
+      </div>
+</form>
+<form method="post" action="don.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="Add Donator">  
+      <hr>
+      </div>
+</form>
+</body>
+</html
