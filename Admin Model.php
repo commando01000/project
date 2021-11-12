d7efcb9ef41f003e38b7fe99d4f37e1a9bdf9180
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-11-12 16:53:35 +0200

    Update Admin Model.php

--- Admin Model.php
+++ Admin Model.php
@@ -1,12 +1,13 @@
 <?php
-class admins
-    { //Mortaga
+    class usertype
+    {
         public $id;
         public $name;
-        public $usertypeObj; 
-        public $username;
-        public $password;
-        function __construct($id)
+        function __construct()
+        {
+            
+        }  
+        function convert_Admin($id)
         {
             if(!$id == "")
             {
@@ -19,26 +20,96 @@ class admins
                 $UserDataSet = mysqli_query($con,$sql);
                 if($row = mysqli_fetch_array($UserDataSet))
                 {
-                    $this->name=$row["name"];
+                    $this->name = $row["name"];
                     $this->id = $id;
-                    $this->username=$row["username"];
-                    $this->password =$row["password"];
-                    $this->usertypeObj= new usertype($row["usertype"]);
                 }
             }
         }
-        function insert($id,$name,$username,$password)
+        function delete($id,$name)
         {
-            $con = mysqli_connect("localhost","root","","project");
-            $sql="SELECT * FROM admins WHERE id = $id or name = $name or username = $username or password = $password";
-            $row = mysqli_query($con,$sql);
-            $num = mysqli_num_rows($row);
-            if($num = 0)
+            $link = mysqli_connect("localhost", "root", "", "project");
+            $sql = "DELETE FROM Register WHERE namee = '$name' or id = '$id'";
+            $run = mysqli_query($con,$sql) or die (mysqli_error($con));
+            echo "Record have been deleted successfully !";
+        }
+        function update ($id,$name)
+        {
+           $query = "SELECT * from card_rec where id='$id'";
+           $run = mysqli_query($con,$query) or die (mysqli_error($con));
+           $query1="UPDATE card_rec SET name='$name', id='$id' WHERE id='$id'";
+
+            if (mysqli_query($con, $query1)) 
             {
-                $sql = "INSERT INTO admins(name,username,password) VALUES('$name','$username','$password')";
-                mysqli_query($con,$sql) or die(mysql_error());
+               echo "updated";
+            }
+            else 
+            {
+                echo "not updated";
             }
         }
-      
+    }
+    class admin
+    {
+        public $id;
+        public $name;
+        public $username;
+        public $password;
+        public $usertypeObj;
+        function __construct()
+        {
+            
+        }
+        function Convert_Admin($id)
+        {
+            if(!$id == "")
+            {
+                $con = mysqli_connect("localhost","root","","project");
+                if(!$con)
+                {
+                    die('Could not connect:' . mysqli_error());
+                }
+                $sql="SELECT * FROM admins WHERE id = $id";
+                $UserDataSet = mysqli_query($con,$sql);
+                if($row = mysqli_fetch_array($UserDataSet))
+                {
+                    $this->name=$row["namee"];
+                    $this->id = $id;
+                    $this->username=$row["username"];
+                    $this->password =$row["passwordd"];
+                    $this->usertypeObj = new usertype($row["usertypeID"]);
+                }
+            }
+        }
+        function insert($name,$username,$password,$type)
+        {
+            $con = mysqli_connect("localhost","root","","project");
+            if(!$con)
+            {
+                die('Could not connect:' . mysqli_error());
+            }
+            else
+            {
+                $sql = "SELECT * FROM admins WHERE namee = '$name' or username = '$username' or passwordd = '$password'";
+                $row = mysqli_query($con,$sql);
+                $num = mysqli_num_rows($row);
+                if($num == 0)
+                {
+                    if($type == "Servent")
+                    {
+                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',2,'$username','$password')";
+                    }
+                    if($type == "Supervisor")
+                    {
+                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',1,'$username','$password')";
+                    }
+                    if($type == "Head Of Servents")
+                    {
+                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',3,'$username','$password')";
+                    }
+                }
+                mysqli_query($con,$sql) or die(mysql_error());
+                echo "Registered Successfully ! ";
+            }
+       }
     }
 ?>
