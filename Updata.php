ca714c85817d9e32ee169c6b2e2f9e99247d380a
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-12 18:06:42 +0200

    Add files via upload

---
+++ Updata.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+<body>  
+
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
+
+<form method="post" action="regFP.php">
+<div class="container">
+<h1>Choose id </h1>
+<hr>
+<input type="text" placeholder="Enter Id" name="id"required>
+<input type="submit" class="registerbtn" name="submit" value="Make profile for patient">   
+<hr>
+</div>
+</form>
+
+</body><!-- Mortaga -->
+</html
