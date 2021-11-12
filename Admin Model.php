38533d43e71b3ad74b5bdbda1381a64aadefef12
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-12 15:50:40 +0200

    Update Admin Model.php

--- Admin Model.php
+++ Admin Model.php
@@ -1,8 +1,11 @@
+<?php
 class admins
-    {
+    { //Mortaga
         public $id;
         public $name;
         public $usertypeObj; 
+        public $username;
+        public $password;
         function __construct($id)
         {
             if(!$id == "")
@@ -18,8 +21,24 @@ class admins
                 {
                     $this->name=$row["name"];
                     $this->id = $id;
+                    $this->username=$row["username"];
+                    $this->password =$row["password"];
                     $this->usertypeObj= new usertype($row["usertype"]);
                 }
             }
         }
+        function insert($id,$name,$username,$password)
+        {
+            $con = mysqli_connect("localhost","root","","project");
+            $sql="SELECT * FROM admins WHERE id = $id or name = $name or username = $username or password = $password";
+            $row = mysqli_query($con,$sql);
+            $num = mysqli_num_rows($row);
+            if($num = 0)
+            {
+                $sql = "INSERT INTO admins(name,username,password) VALUES('$name','$username','$password')";
+                mysqli_query($con,$sql) or die(mysql_error());
+            }
+        }
+      
     }
+?>
