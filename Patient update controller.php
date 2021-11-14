de03bd62f25324459a52d115dc43f3f0985c0694
JohnSameh7 <92456160+JohnSameh7@users.noreply.github.com>
2021-11-14 17:37:58 +0200

    Add files via upload

---
+++ Patient update controller.php
@@ -0,0 +1,23 @@
+<?php
+    include_once 'Patient Model.php';
+    include_once 'Patient View.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        $name = $Gender = $Age = $Job = $Socialstatus = $NumberInFamily = $Address = $MobileNumber = $HomeNumber = $SupervisedBy = $PatientComplain = $Complain = "";
+        $name = trim($_POST["Name"]);
+        $Gender = trim($_POST["gender"]);
+        $Age = trim($_POST["age of patient"]);
+        $Job = trim($_POST["patient job"]);
+        $Socialstatus = trim($_POST["Enter Social status"]);
+        $NumberInFamily = trim($_POST["rank"]);
+        $Address = trim($_POST["Adress"]);
+        $MobileNumber = trim($_POST["mobile no."]);
+        $HomeNumber = trim($_POST["Home no."]);
+        $SupervisedBy = trim($_POST["Servant name"]);
+        $PatientComplain = trim($_POST["patient Complaint"]);
+        $Complain = trim($_POST["Complaints of family and companions"]);
+        $UpdateAdmin = new admin();
+        $UpdateAdmin->Update($name , $Gender , $Age , $Job , $MaritalStatus , $NumberInFamily , $Address , $MobileNumber , $HomeNumber , $SupervisedBy , $PatientComplain , $Complain );
+        header('Location:afterlogin.php');
+    }
+?>
