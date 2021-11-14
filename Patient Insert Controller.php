69e5a6bdb42d07f598ca6db229a1c21564a6d649
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2021-11-14 20:16:05 +0200

    Add files via upload

---
+++ Patient Insert Controller.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'Patient Model.pphp';
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $NAME = $Gender = $Age = $Job = $MaritalStatus = $NumberInFamily = $Address = $PhoneNumber = $HomeNumber = $SupervisedBy = $PatientComplain = $Complain = "";
+    $NAME = trim($_POST["name of patient"]);
+    $Gender = trim($_POST["gender"]);
+    $Age = trim($_POST["age of patient"]);
+    $Job = trim($_POST["patient job"]);
+    $MaritalStatus = trim($_POST["Enter Social status"]);
+    $NumberInFamily = trim($_POST["rank"]);
+    $Address = trim($_POST["Adress"]);
+    $PhoneNumber = trim($_POST["mobile number"]);
+    $HomeNumber = trim($_POST["Home number"]);
+    $SupervisedBy = trim($_POST["Supervised by"]);
+    $PatientComplain = trim($_POST["patient Complaint"]);
+    $Complain = trim($_POST["Complaints of family and companions"]);
+    
+    $InsertPatient = new patient();
+    $InsertPatient->Insert($NAME,$Gender,$Age,$Job,$MaritalStatus,$NumberInFamily,$Address,$PhoneNumber,$HomeNumber,$SupervisedBy,$PatientComplain,$Complain);
+    header("Location: afterlogin.php");
+}
+?>
