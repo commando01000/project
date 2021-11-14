c5b420ef352eb6fc4ca59e1108b44940db499057
Andrew17892 <92439465+Andrew17892@users.noreply.github.com>
2021-11-14 23:34:28 +0200

    Add files via upload

---
+++ donation.js
@@ -0,0 +1,12 @@
+
+  function mathod();
+  {
+
+  
+    var list = document.getElementById('Items').value
+    alert ("afsf")
+    if(list.value==="Money")
+    {
+        document.getElementById('a').innerHTML = 'Heading changed with JS';
+    }
+  }
