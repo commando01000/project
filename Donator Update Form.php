f8873b020e767ebcbe132e3aa8aca2437ab964a8
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2021-11-15 21:50:07 +0200

    Add files via upload

---
+++ Donator Update Form.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+</head>
+<body>  
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="afterlogin">Choose your action</a></li>
+  </ul>
+<form method="post" action="Donator Update Controller">
+<div class="container">
+<h1>Make profile for Donator</h1>
+<hr>
+  ID: <input type="text" placeholder="Enter ID of Donator" name="ID of donator"required>
+  <br><br>
+  New Amount: <input type="number" placeholder="Enter new amount" name="new amount"required>
+  <br><br>
+      <input type="submit" class="registerbtn" name="submit" value="Update">  
+      <hr>
+  <hr>
+</div>
+</form>
+</body>
+</html>
