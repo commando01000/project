00377acc877cb02f071565a06a73e55c55f2d581
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2021-11-16 00:33:52 +0200

    Create FrontEnndDonatorView.php

---
+++ FrontEnndDonatorView.php
@@ -0,0 +1,26 @@
+
+<head>
+</head>
+<body>  
+
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="afterlogin">Choose your action</a></li>
+  </ul>
+
+<form method="post" action = "DonationDetailsView.php">
+<div class="container">
+<h1>Choose id </h1>
+<hr>
+<input type="text" placeholder="Enter Id" name="Submit"required>
+<input type="submit" class="registerbtn" name="submit" value="Submit">   
+<hr>
+</div>
+</form>
+
+</body><!-- Mortaga -->
+</html
