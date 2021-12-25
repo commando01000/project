8db316a1865fb0a7976b46869838609cd2c6f491
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2021-12-25 17:33:19 +0200

    Update Admin View.php

--- Admin View.php
+++ Admin View.php
@@ -5,7 +5,7 @@ class Adminview()
     {
         echo "id: ".$Admin->id;
         echo "name: ".$Admin->name;
-        echo "usertypeID ".$Admin->usertypeObj;
+        echo "usertypeID ".$Admin->usertypeObj->name;
         echo "username ".$Admin->username;
         echo "passwordd ".$Admin->password;
     }
