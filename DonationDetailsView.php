6f60f83e6021375dcc277b5f396f830211af0732
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-25 17:51:06 +0200

    Delete DonationDetailsView.php

--- DonationDetailsView.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DonationDetailsView
-{
-    public function ShowDonationDetails($DonationDetails)
-    {
-        echo "name:".$DonationDetails->donator_id->name . "amount".$DonationDetails->amount . "item ".$DonationDetails->item;
-    }
-}
-?>
