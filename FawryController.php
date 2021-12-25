99d165d1706e30f8b14d5110c572f0c102bd01f1
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-25 21:48:07 +0200

    Update FawryController.php

--- FawryController.php
+++ FawryController.php
@@ -6,16 +6,16 @@ include_once 'Fawry.php';
 include_once 'PayType.php';
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
-        $Code = $AccountNumber = $Name="";
+        $Code = $AccountNumber = $Name = "";
         $Code = trim($_POST["Code"]);
         $AccountNumber = trim($_POST["AccountNumber"]);
-        $Name= trim($_POST["name"]);
+        $id = trim($_POST["id"]);
         $CreateFactory = new FactoryDP();
+        $ArrObj;
         $ArrObj[0] = $Code;
         $ArrObj[1] = $AccountNumber;
-        $ArrObj[2] = $Name;
-        $fawry = $CreateFactory->get_PayType(2);
-        $fawry->Pay($ArrObj);
-        header("Location: afterlogin.php");
+        $ArrObj[2] = $id;
+        $Fawry = $CreateFactory->get_PayType(2);
+        $Fawry->Pay($ArrObj);
     }
 ?>
