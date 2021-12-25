e3c73510b3a3397cbe45cafe0a5a491e1a26a995
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-25 21:47:34 +0200

    Update Fawry.php

--- Fawry.php
+++ Fawry.php
@@ -1,36 +1,22 @@
+
+
+
+
+
+
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/** 
- * Description of Fawry
- *
- * @author Dell
- */
 include_once 'PayType.php';
 include_once 'FawryController.php';
-class Fawry implements PayType{
+include_once 'user.php';
+class Fawry implements PayType {
     public function Pay($ArrObj) {
         $con = mysqli_connect("localhost", "root", "", "project 3");
-        $sql = "SELECT * from donator where name = '$ArrObj[2]'";
-        $row = mysqli_query($con,$sql);
-        $user_id = $row["user_id"];
-        $num = mysqli_num_rows($row);
-        if($num >= 1)
-        { 
-            $sql = "INSERT into paymentmethodoptionsvalue(PaymentMethod_ID,PaymentMethodOptionID,user_id,Value) VALUES(2,7,'$user_id','$ArrObj[0]')";
-            $go = mysqli_query($con,$sql) or die (mysqli_error($con));
-            $sql = "INSERT into paymentmethodoptionsvalue(PaymentMethod_ID,PaymentMethodOptionID,user_id,Value) VALUES(2,8,'$user_id','$ArrObj[1]')";
-            $go = mysqli_query($con,$sql) or die (mysqli_error($con));
-        }
-        else
-        {
-            echo "Donator not found !";
-        }   
+        $id = $ArrObj[2];
+        $user_id = $id;
+        $sql = "INSERT into paymentmethodoptionsvalue(PaymentMethod_ID,PaymentMethodOptionID,user_id,Value) VALUES(2,7,'$user_id','$ArrObj[0]')";
+        $go = mysqli_query($con,$sql) or die (mysqli_error($con));
+        $sql2 = "INSERT into paymentmethodoptionsvalue(PaymentMethod_ID,PaymentMethodOptionID,user_id,Value) VALUES(2,6,'$user_id','$ArrObj[1]')";
+        $go2 = mysqli_query($con,$sql2) or die (mysqli_error($con));
     }
 }
-
+?>
