1b25a1403ec8b3bd5308f934f5b7bbd8b14b41b8
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-25 17:27:27 +0200

    Update Admin Model.php

--- Admin Model.php
+++ Admin Model.php
@@ -1,144 +1,171 @@
 <?php
+    include_once 'user.php';
     class usertype
     {
         public $id;
         public $name;
-        function __construct()
+        function __construct($id)
         {
-            
-        }  
-        function convert_Admin($id)
-        {
-            if(!$id == "")
+            if($id != -1)
             {
-                $con = mysqli_connect("localhost","root","","project");
+                $con = mysqli_connect("localhost","root","","project 3");
                 if(!$con)
                 {
                     die('Could not connect:' . mysqli_error());
                 }
-                $sql="SELECT * FROM admins WHERE id = $id";
-                $UserDataSet = mysqli_query($con,$sql);
-                if($row = mysqli_fetch_array($UserDataSet))
-                {
-                    $this->name = $row["name"];
-                    $this->id = $id;
-                }
-            }
-        }
-        function delete($id,$name)
-        {
-            $link = mysqli_connect("localhost", "root", "", "project");
-            $sql = "DELETE FROM admins WHERE namee = '$name' or id = '$id'";
-            $run = mysqli_query($link,$sql) or die (mysqli_error($con));
-            echo "Record have been deleted successfully !";
-        }
-        function update ($id,$name)
-        {
-           $link = mysqli_connect("localhost", "root", "", "project");
-           $query = "SELECT * from admins where id='$id'";
-           $run = mysqli_query($link,$query) or die (mysqli_error($con));
-           $query1="UPDATE admins SET name='$name', id='$id' WHERE id='$id'";
-
-            if (mysqli_query($con, $query1)) 
-            {
-               echo "updated";
-            }
-            else 
-            {
-                echo "not updated";
-            }
-        }
-    }
-    class admin
-    {
-        public $id;
-        public $name;
-        public $username;
-        public $password;
-        public $usertypeObj;
-        function __construct()
-        {
-            
-        }
-        function Convert_Admin($id)
-        {
-            if(!$id == "")
-            {
-                $con = mysqli_connect("localhost","root","","project");
-                if(!$con)
-                {
-                    die('Could not connect:' . mysqli_error());
-                }
-                $sql="SELECT * FROM admins WHERE id = $id";
-                $UserDataSet = mysqli_query($con,$sql);
-                if($row = mysqli_fetch_array($UserDataSet))
-                {
-                    $this->name=$row["namee"];
-                    $this->id = $id;
-                    $this->username=$row["username"];
-                    $this->password =$row["passwordd"];
-                    $this->usertypeObj = new usertype($row["usertypeID"]);
-                }
+                $sql = "SELECT * FROM user_type WHERE id = $id";
+                $s = mysqli_query($con,$sql);
+                $row = mysqli_fetch_array($s);
+                $this->id = $id;
+                $this->name = $row["name"];
             }
         }
         function delete($id)
         {
-            $con = mysqli_connect("localhost", "root", "", "project");
+            $con = mysqli_connect("localhost", "root", "", "project 3");
             $sql = "SELECT * from admins where id = '$id'";
             $row = mysqli_query($con,$sql);
             $num = mysqli_num_rows($row);
             if($num == 1)
             { 
-                $sql = "DELETE FROM admins WHERE  id = '$id'";
+                $sql = "DELETE FROM user_type WHERE  id = '$id'";
                 $go = mysqli_query($con,$sql) or die (mysqli_error($con));
             }
             else
             {
                 echo "user not found !";
             }
+            $con->close();
         }
-         function update($id,$name,$username,$password)
+        function update($id,$name)
         {
-            $con = mysqli_connect("localhost", "root", "", "project");
-            $sql = "SELECT * from admins where id='$id'";
+            $con = mysqli_connect("localhost", "root", "", "project 3");
+            $sql = "SELECT * from user_type where id ='$id'";
             $row = mysqli_query($con,$sql);
             $num = mysqli_num_rows($row);
             if($num == 1)
-            {
-                $go = mysqli_query($con,$sql) or die (mysqli_error($con));  
-                $sql2 = "UPDATE admins SET namee ='$name' , username = '$username' , passwordd = '$password' WHERE id ='$id'";
-                $go = mysqli_query($con,$sql2) or die (mysqli_error($con));
+            {  
+                $sql = "UPDATE user_type SET name ='$name' WHERE id ='$id'";
+                $go = mysqli_query($con,$sql) or die (mysqli_error($con));
             }
             else
             {
                 echo "user not found !";
             }
+            $con->close();
         }
-        function insert($name,$username,$password,$type)
+        function insert($name)
         {
-            $con = mysqli_connect("localhost","root","","project");
+            $con = mysqli_connect("localhost","root","","project 3");
             if(!$con)
             {
                 die('Could not connect:' . mysqli_error());
             }
             else
             {
-                $sql = "SELECT * FROM admins WHERE namee = '$name' or username = '$username' or passwordd = '$password'";
+                $sql = "SELECT * FROM user_type WHERE name = '$name'";
+                $row = mysqli_query($con,$sql);
+                $num = mysqli_num_rows($row);
+                if($num == 0)
+                {
+                    $sql = "INSERT INTO user_type(name) VALUES('$name')";
+                }
+                else
+                {
+                    echo "user already exists ! ";
+                }
+                mysqli_query($con,$sql) or die(mysql_error());
+                echo "Registered Successfully ! ";
+            }
+            $con->close();
+        }
+    }
+    class admin
+    {
+        public $id;
+        public $username;
+        public $password;
+        public $usertype_id;
+        function __construct($id)
+        {
+            if($id != -1)
+            {
+                $con = mysqli_connect("localhost","root","","project 3");
+                if(!$con)
+                {
+                    die('Could not connect:' . mysqli_error());
+                }
+                $sql = "SELECT * FROM admin WHERE id = $id";
+                $S = mysqli_query($con,$sql);
+                if($row = mysqli_fetch_array($S))
+                {
+                    $this->id = $id;
+                    $this->username = $row["username"];
+                    $this->password = $row["password"];
+                    $this->usertype_id = new usertype($row["usertype_id"]);
+                }
+                $con->close();
+            }
+        }
+        function delete($id)
+        {
+            $con = mysqli_connect("localhost", "root", "", "project 3");
+            $sql = "SELECT * from admin where id = '$id'";
+            $row = mysqli_query($con,$sql);
+            $num = mysqli_num_rows($row);
+            if($num == 1)
+            { 
+                $sql = "DELETE FROM admin WHERE  id = '$id'";
+                $go = mysqli_query($con,$sql) or die (mysqli_error($con));
+            }
+            else
+            {
+                echo "user not found !";
+            }
+            $con->close();
+        }
+        function update($id,$username,$password,$usertype_id)
+        {
+            $con = mysqli_connect("localhost", "root", "", "project 3");
+            $sql = "SELECT * from admin where id ='$id'";
+            $row = mysqli_query($con,$sql);
+            $num = mysqli_num_rows($row);
+            if($num == 1)
+            {  
+                $sql2 = "UPDATE admin SET username = '$username' , password = '$password' ,usertype_id = '$usertype_id' WHERE id ='$id'";
+                $go = mysqli_query($con,$sql2) or die (mysqli_error($con));
+            }
+            else
+            {
+                echo "user not found !";
+            }
+            $con->close();
+        }
+        function insert($username,$password,$type)
+        {
+            $con = mysqli_connect("localhost","root","","project 3");
+            if(!$con)
+            {
+                die('Could not connect:' . mysqli_error());
+            }
+            else
+            {
+                $sql = "SELECT * FROM admin WHERE username = '$username' or password = '$password'";
                 $row = mysqli_query($con,$sql);
                 $num = mysqli_num_rows($row);
                 if($num == 0)
                 {
                     if($type == "Servent")
                     {
-                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',2,'$username','$password')";
+                        $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',2)";
                     }
                     if($type == "Supervisor")
                     {
-                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',1,'$username','$password')";
+                        $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',1)";
                     }
                     if($type == "Head Of Servents")
                     {
-                        $sql = "INSERT INTO admins(namee,usertypeID,username,passwordd) VALUES('$name',3,'$username','$password')";
+                        $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',3)";
                     }
                 }
                 else
@@ -148,6 +175,8 @@
                 mysqli_query($con,$sql) or die(mysql_error());
                 echo "Registered Successfully ! ";
             }
-       }
+            $con->close();
+        }
     }
 ?>
+
