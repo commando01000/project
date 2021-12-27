4bf1025ce4c124eb767d27194e70e1040faad58f
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-27 19:44:09 +0200

    Update BankTransfer.php

--- BankTransfer.php
+++ BankTransfer.php
@@ -14,15 +14,11 @@
 include_once 'PayType.php';
 class BankTransfer implements PayType{
     public function Pay($ArrObj) {
-        $con = mysqli_connect("localhost","root","","project 3");
-        if(!$con)
-        {
-            die('Could not connect:' . mysqli_error());
-        }
-        else
-        {
-           
-        }
+        $con = mysqli_connect("localhost", "root", "", "project 3");
+        $id = $ArrObj[0];
+        $user_id = $id;
+        $sqll = "INSERT INTO paymentmethodoptionsvalue(PaymentMethod_ID,PaymentMethodOptionID,user_id,Value) VALUES(4,6,'$user_id','$ArrObj[1]')";
+        $exec = mysqli_query($con,$sqll) or die (mysqli_error($con));
         $con->close();
     }
 }
