019760b604e845e05fac37f0c74b2c250cf2d5d7
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2021-12-27 19:40:28 +0200

    Update FawryController.php

--- FawryController.php
+++ FawryController.php
@@ -10,12 +10,24 @@ include_once 'PayType.php';
         $Code = trim($_POST["Code"]);
         $AccountNumber = trim($_POST["AccountNumber"]);
         $id = trim($_POST["id"]);
-        $CreateFactory = new FactoryDP();
         $ArrObj;
         $ArrObj[0] = $Code;
         $ArrObj[1] = $AccountNumber;
         $ArrObj[2] = $id;
-        $Fawry = $CreateFactory->getFawry();
-        $Fawry->Pay($ArrObj);
+        $con = mysqli_connect("localhost", "root", "", "project 3");
+        $sql = "SELECT * FROM donator WHERE user_id = '$id'";
+        $row = mysqli_query($con, $sql);
+        $num = mysqli_num_rows($row);
+        if($num > 0)
+        {
+            $Fawry = $CreateFactory->getFawry();
+            $Fawry->Pay($ArrObj);
+            header("Location: afterlogin.php");
+        }
+        else
+        {
+           echo '<script>alert("user is not registered as a donator !")</script>';
+        }
+        $con->close();
     }
 ?>
