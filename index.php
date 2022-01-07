00e368c46fe918fb9b7471edc77ecf379e520637
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2022-01-07 17:07:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-<head>
-</head>
-<body>  
-  
-<head>
-  <link rel="stylesheet" href="cssF.css">
-</head>
-
-<ul>
-  <li><a href="index"></a></li>
-  <li><a href="#news">  </a></li>
-  <li><a href="#contact">  </a></li>
-  </ul>
-  
-
-
-<form method="post" action="LoginFA.php">
-<div class="container">
-  
-<h3>Home</h3>
-
-
-<hr>
-  
-  <input type="submit" class="registerbtn" name="submit" value="Sign in">  
-  <hr>
-  </div>
-</form>
-<form method="post" action="regFA.php">
-    <div class="container">
-    <hr>
-      <input type="submit" class="registerbtn" name="submit" value="Add Servant">  
-      <hr>
-      </div>
-</form>
-
-</body><!-- Mortaga -->
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            include_once 'Context.php';
+            include_once 'FormState.php';
+            include_once 'AfterFormState.php';
+            include_once 'State.php';
+            $Do=new Context();
+            
+            $x = new FormState();
+            $x->proccesse($Do);
+            echo $Do->getDescription() . "<br>";
+            
+            $x = new AfterFormState();
+            $x->proccesse($Do);
+            echo $Do->getDescription() . "<br>";
+        ?>
+    </body>
 </html>
