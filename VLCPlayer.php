a0f40f8f4b5ca45858d68a68b8e34dfd92f0dad5
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2022-01-24 02:17:24 +0200

    Add files via upload

---
+++ VLCPlayer.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of VLCPlayer
+ *
+ * @author Andrew
+ */
+include_once 'AdvancedMediaPlayer.php';
+class VLCPlayer implements AdvancedMediaPlayer{
+    
+    public function playMKV() {
+        
+    }
+
+    public function playMP4() {
+        
+    }
+
+    public function playVLC() {
+        echo "playing VLC Player" . "<br>";
+    }
+
+    public function playWEBM() {
+        
+    }
+}
+?>
