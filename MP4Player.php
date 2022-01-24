3a2424baa70f180cd24a37aad3b148ff8a1d9ea9
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2022-01-24 02:17:00 +0200

    Add files via upload

---
+++ MP4Player.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MP4Player
+ *
+ * @author Andrew
+ */
+include_once'AdvancedMediaPlayer.php';
+class MP4Player implements AdvancedMediaPlayer{
+    
+    public function playMKV() {
+        
+    }
+
+    public function playMP4() {
+        echo "Playing MP4 Player" . "<br>";
+    }
+
+    public function playVLC() {
+        
+    }
+
+    public function playWEBM() {
+        
+    }
+}
+?>
+
