6bcd52cb543ddb7f588c8eb386594af4820ca85e
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2022-01-24 02:14:01 +0200

    Add files via upload

---
+++ AdvancedMediaPlayer.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+interface AdvancedMediaPlayer {
+    public function playVLC();
+    public function playMP4();
+    public function playWEBM();
+    public function playMKV();
+}
+?>
