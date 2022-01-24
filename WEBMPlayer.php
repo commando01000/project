bb27e721b9922905d61425a2079bb0a3f579213d
Andrew-120 <92456025+Andrew-120@users.noreply.github.com>
2022-01-24 02:18:00 +0200

    Add files via upload

---
+++ WEBMPlayer.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of WEBMPlayer
+ *
+ * @author Andrew
+ */
+include_once 'AdvancedMediaPlayer.php';
+class WEBMPlayer implements AdvancedMediaPlayer{
+    public function playMKV() {
+        
+    }
+
+    public function playMP4() {
+        
+    }
+
+    public function playVLC() {
+        
+    }
+
+    public function playWEBM() {
+        echo "Playing WEBM Player" . "<br>";
+    }
+
+}
