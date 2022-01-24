02d14126ff11ee7808b9c5f720374b182320afec
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2022-01-24 13:23:19 +0200

    Add files via upload

---
+++ afterlogin2.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+</head>
+<body>  
+
+<head>
+  <link rel="stylesheet" href="cssF.css">
+</head>
+<ul>
+  <li><a href="index">Home</a></li>
+  <li><a href="LoginFA">Login</a></li>
+  <li><a href="#contact">  </a></li>
+  </ul>
+<form method="post" action="userview.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="Show User">
+      <hr>
+      </div>
+</form>
+<form method="post" action="ShowAllUsersController.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="Show All Users">
+      <hr>
+      </div>
+</form>
+  <form method="post" action="Front End Admin View.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="View Admin">
+      <hr>
+      </div>
+</form>
+<form method="post" action="PaymentMethodOptValueHTML.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="View All Transactions">
+      <hr>
+      </div>
+</form>
+<form method="post" action="PaymentMethodOptHTML.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="View Payment Method Options">
+      <hr>
+      </div>
+</form>
+  <form method="post" action="FrontEndDonatorView.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="View Donator">  
+      <hr>
+      </div>
+</form>
+  <form method="post" action="Donator_ViewAll_Controller.php">
+    <div class="container">
+    <hr>
+      <input type="submit" class="registerbtn" name="submit" value="View All Donator">  
+      <hr>
+      </div>
+</form>
+</body><!-- Mahmoud -->
+</html
