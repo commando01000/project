e835ef769149dcc1dcd64591cfcfd1990c8407e5
Mahmoud Ebrahim Mohamed <92318302+commando01000@users.noreply.github.com>
2022-01-26 02:15:47 +0200

    Update Admin View.php

--- Admin View.php
+++ Admin View.php
@@ -3,11 +3,9 @@ class Adminview()
 {
     function ShowAdminDetails($Admin)
     {
-        echo "id: ".$Admin->id;
-        echo "name: ".$Admin->name;
-        echo "usertypeID ".$Admin->usertypeObj->name;
-        echo "username ".$Admin->username;
-        echo "passwordd ".$Admin->password;
+        echo "username:". " " . $Admin->username . " ";
+        echo "usertypeID:". " " .$Admin->usertype_id->name . " ";
+        echo "password:". " " . $Admin->password . " ";
     }
 }
 ?>
