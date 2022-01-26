5860fc15bc0be344f4ea45e63096164d5bff9f17
Andrew17892 <92439465+Andrew17892@users.noreply.github.com>
2022-01-26 13:05:08 +0200

    Add files via upload

--- facade.php
+++ facade.php
@@ -1,75 +1,57 @@
-<php
-class pagefacade
+//this code is made java i know
+public interface facade {
+   
+    public function bearing ratio();
+}
+public class home implies facade()
 {
-
-    
-
-    public function createandvalidate($username,$password,$type)
+    public function bearing ratio();
     {
-        $a=0;
-        
-        $username = $password = $type = "";
-        $username = trim($_POST["username"]);
-        $password = trim($_POST["password"]);
-        $type = trim($_POST["job"]);
-
-        if ($typeofcheck == "1")
-        {
-            $uppercase = preg_match('@[A-Z]@', $password);
-            $lowercase = preg_match('@[a-z]@', $password);
-            $number    = preg_match('@[0-9]@', $password);
-            $specialChars = preg_match('@[^\w]@', $password);
-
-            if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) 
-            {
-                echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
-            }
-            else
-            {
-                $a=1;
-            }
-        } 
-
-         
-            $con = mysqli_connect("localhost","root","","project 3");
-            if(!$con)
-            {
-                die('Could not connect:' . mysqli_error());
-            }
-            else
-            {
-                if($a==1)
-                {
-                    $sql = "SELECT * FROM admin WHERE username = '$username' or password = '$password'";
-                    $row = mysqli_query($con,$sql);
-                    $num = mysqli_num_rows($row);
-                    if($num == 0)
-                    {
-                        if($type == "Servent")
-                        {
-                            $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',2)";
-                        }
-                        if($type == "Supervisor")
-                        {
-                            $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',1)";
-                        }
-                        if($type == "Head Of Servents")
-                        {
-                            $sql = "INSERT INTO admin(username,password,usertype_id) VALUES('$username','$password',3)";
-                        }
-                    }
-                    else
-                    {
-                        echo "user already exists ! ";
-                    }
-                    mysqli_query($con,$sql) or die(mysql_error());
-                    echo "Registered Successfully ! ";
-                }
-            }
-            $con->close();
-        
-         
+        System.out.println(" home bearing");
 
     }
+
 }
-?>
+public class donor implies facade()
+{
+    public function bearing ratio();
+    {
+        System.out.println(" donor bearing");
+
+    }
+
+}
+public class organization implies facade()
+{
+    public function bearing ratio();
+    {
+        System.out.println(" organization bearing");
+
+    }
+
+}
+public class typebearer()
+{
+    private facade home;
+    private facade donor;
+    private facade organization;
+    public typebearer ()
+    {
+        home= new home();
+        rectangle = new rectangle()
+        organization = new organization()
+    }
+    public void bearer home()
+    {
+        home.bearing ratio();
+    }
+    public void bearer donor()
+    {
+        donor.bearing ratio();
+    }
+    public void bearer organization()
+    {
+        organization.bearing ratio();
+    }
+
+}
